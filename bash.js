6e5859bd925dbd19186429afc76b2f49cee26f30
Anshu Patel <anshupatel314@gmail.com>
2021-09-24 15:50:31 -0400

    added cat cmd

--- bash.js
+++ bash.js
@@ -1,19 +1,21 @@
-const pwd = require('./pwd')
-const ls = require('./ls');
+const pwd = require("./pwd");
+const ls = require("./ls");
+const cat = require("./cat");
 
 // Output a prompt
 process.stdout.write("prompt > ");
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on("data", (data) => {
-
   const cmd = data.toString().trim(); // remove the newline
   if (cmd === "pwd") {
     pwd();
-  } else if (cmd === 'ls') {
+  } else if (cmd === "ls") {
     ls();
+  } else if (cmd.includes("cat")) {
+    const fileName = cmd.split(" ")[1];
+    cat(fileName);
   } else {
     process.stdout.write("You typed: " + cmd);
   }
-  process.stdout.write("\nprompt > ");
 });
--- ls.js
+++ ls.js
@@ -1,10 +1,12 @@
-const fs = require('fs');
+const fs = require("fs");
 
-module.exports = fs.readdir('./', 'utf8', (err, files) => {
+module.exports = function () {
+  fs.readdir("./", "utf8", (err, files) => {
     if (err) {
-        throw err
+      throw err;
     } else {
-        process.stdout.write(files.join('\n'))
-        process.stdout.write('\nprompt >');
+      process.stdout.write(files.join("\n"));
+      process.stdout.write("\nprompt > ");
     }
-})
+  });
+};
